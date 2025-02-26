4b557aba6e35ae27b76f652b1199524673d227dd
franciscocamellon <franciscocamellon@gmail.com>
2025-02-26 08:42:23 -0300

    Initial code

---
+++ src/main/java/br/edu/infnet/pageflow/PageFlowApplication.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.pageflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PageFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PageFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/infnet/pageflow/PageFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.pageflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PageFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
