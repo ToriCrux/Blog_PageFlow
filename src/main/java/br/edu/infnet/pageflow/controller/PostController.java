d095140bc07357eddad9fc733f9b29164e522d60
Raphael Van Well <rapha.well@outlook.com>
2025-03-02 11:02:33 -0300

    [Feature] Criar CRUD de Postagens Criar endpoints para criação de postagens e listagem de postagens Criar endpoints para edição e exclusão de postagens Atualizar PostController para gerenciar edição e exclusão de postagens Criar PostService para implementar regras de edição e exclusão de postagens Permitir que postagens sejam associadas a um Author

--- src/main/java/br/edu/infnet/pageflow/controller/PostController.java
+++ src/main/java/br/edu/infnet/pageflow/controller/PostController.java
@@ -1,6 +1,7 @@
 package br.edu.infnet.pageflow.controller;
 
 import br.edu.infnet.pageflow.model.Post;
+import br.edu.infnet.pageflow.model.PostRequest;
 import br.edu.infnet.pageflow.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -22,7 +23,25 @@ public class PostController {
     }
 
     @PostMapping("/new")
-    public ResponseEntity<Post> createPost(@RequestBody Post post) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(postService.createPost(post));
+    public ResponseEntity<Post> createPost(@RequestBody PostRequest postRequest) {
+        if (postRequest.getAuthorId() == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+
+        Post post = new Post(postRequest.getTitle(), postRequest.getContent(), null);
+        Post createdPost = postService.createPost(post, postRequest.getAuthorId());
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdPost);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Post> updatePost(@PathVariable Integer id, @RequestBody Post updatedPost) {
+        return ResponseEntity.ok(postService.updatePost(id, updatedPost));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletePost(@PathVariable Integer id) {
+        postService.deletePost(id);
+        return ResponseEntity.noContent().build();
     }
 }
---
+++ src/main/java/br/edu/infnet/pageflow/model/PostRequest.java
@@ -0,0 +1,32 @@
+package br.edu.infnet.pageflow.model;
+
+public class PostRequest {
+    private String title;
+    private String content;
+    private Integer authorId;
+
+    // Getters e Setters
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Integer getAuthorId() {
+        return authorId;
+    }
+
+    public void setAuthorId(Integer authorId) {
+        this.authorId = authorId;
+    }
+}
