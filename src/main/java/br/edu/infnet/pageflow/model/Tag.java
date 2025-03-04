1d19043dfc54f3d39ed7711c26cd4f649f5383d0
luisanselmobd <luisanselmo014@gmail.com>
2025-03-04 17:51:03 -0300

    Finish task: Create the Tag model

--- src/main/java/br/edu/infnet/pageflow/model/Tag.java
+++ src/main/java/br/edu/infnet/pageflow/model/Tag.java
@@ -1,9 +1,28 @@
 package br.edu.infnet.pageflow.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 import java.util.UUID;
 
 public class Tag {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
 
 }
