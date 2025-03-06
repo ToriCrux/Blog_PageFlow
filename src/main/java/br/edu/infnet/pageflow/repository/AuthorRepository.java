459bf71238b4c8b2df9589ad4d293ef2e9e1fc9c
franciscocamellon <franciscocamellon@gmail.com>
2025-03-06 10:27:20 -0300

    Concentrated user management on UserRepository

--- src/main/java/br/edu/infnet/pageflow/repository/AuthorRepository.java
+++
@@ -1,9 +0,0 @@
-package br.edu.infnet.pageflow.repository;
-
-import br.edu.infnet.pageflow.model.Author;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AuthorRepository extends CrudRepository<Author, Integer> {
-}
