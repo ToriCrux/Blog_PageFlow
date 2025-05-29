a0ad5e26dd01237e8b2cf837d1f130473a121e39
Raphael Van Well <rapha.well@outlook.com>
2025-05-29 12:11:07 -0300

    Add Docker and Docker-compose support

---
+++ PageFlow-front/src/app/Login/apiServiceLogin.ts
@@ -0,0 +1,37 @@
+export interface LoginPayload {
+  email: string;
+  password: string;
+}
+
+export interface LoginResponse {
+  jwt: string;
+}
+
+
+const API_BASE_URL = "http://localhost:8080";
+
+export const loginUsuario = async (payload: LoginPayload): Promise<LoginResponse> => {
+  try {
+    const response = await fetch(`${API_BASE_URL}/auth/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(payload),
+    });
+
+    if (!response.ok) {
+      const errorText = await response.text();
+      throw new Error(errorText || "Erro ao fazer login (resposta inválida).");
+    }
+
+    const result = await response.json();
+    return result;
+
+  } catch (error: any) {
+    if (error.message === "Failed to fetch") {
+      throw new Error("Não foi possível conectar ao servidor. Verifique se a API está rodando.");
+    }
+    throw error;
+  }
+};
