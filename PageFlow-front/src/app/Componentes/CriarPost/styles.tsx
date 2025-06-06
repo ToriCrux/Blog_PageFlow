a0d738eb2976977e58e908f8a85f27112c5a93a9
franciscocamellon <franciscocamellon@gmail.com>
2025-06-05 21:35:09 -0300

    Adding draft button. Fixing icons

--- PageFlow-front/src/app/Componentes/CriarPost/styles.tsx
+++ PageFlow-front/src/app/Componentes/CriarPost/styles.tsx
@@ -46,3 +46,31 @@ export const SendButton = tw.button`
   text-[#9C0D38]
   text-xl
 `;
+export const StyledCheckbox = tw.input`
+  form-checkbox
+  h-5
+  w-5
+  text-[#9C0D38]
+  rounded-full
+  border-gray-300
+  focus:ring-[#9C0D38]
+`;
+
+export const CheckboxLabel = tw.label`
+  flex
+  items-center
+  gap-2
+  text-sm
+  text-[#9C0D38]
+  cursor-pointer
+`;
+export const ToggleCheckbox = tw.button<{ $checked?: boolean }>`
+  ${(p) => (p.$checked ? "bg-[#9C0D38] text-white" : "bg-gray-300 text-[#9C0D38]")}
+  px-4
+  py-2
+  rounded-full
+  flex
+  items-center
+  text-sm
+  transition-colors
+`;
